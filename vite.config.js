838c81dd192ae8bf6327a4d1be920e6b1bfa6414
kdng5 <kevinnguyend0102@gmail.com>
2025-06-27 13:03:40 +0900

    Updated Vite Config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: process.env.NODE_ENV === 'production' ? '/G32-3D-Calculator/' : '',
     plugins: [glsl()]
 });
